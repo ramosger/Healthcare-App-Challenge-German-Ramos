40156d49966b07f3337655357be7b03e72359725
Germán Ramos <german.ramos@lightit.io>
2025-12-01 14:23:37 -0300

    fix: zod schema

--- src/services/auth/schemas.ts
+++ src/services/auth/schemas.ts
@@ -23,15 +23,42 @@ export const getRegisterRequestSchema = () => {
       fullName: z.string().min(4, {
         message: i18n.t("form.errors.required", { field: i18n.t("form.fullName") }),
       }),
-      email: z.email({
-        message: i18n.t("form.errors.invalidField", { field: i18n.t("form.email") }),
-      }),
-      password: z.string().min(6, {
-        message: i18n.t("form.errors.minLength", { field: i18n.t("form.password"), length: 6 }),
-      }),
-      confirmPassword: z.string().min(6, {
-        message: i18n.t("form.errors.minLength", { field: i18n.t("form.password"), length: 6 }),
-      }),
+      email: z
+        .string()
+        .min(1, {
+          message: i18n.t("form.errors.required", {
+            field: i18n.t("form.email"),
+          }),
+        })
+        .email({
+          message: i18n.t("form.errors.invalidField", {
+            field: i18n.t("form.email"),
+          }),
+        }),
+      password: z
+        .string()
+        .min(1, {
+          message: i18n.t("form.errors.required", {
+            field: i18n.t("form.password"),
+          }),
+        })
+        .min(8, {
+          message: i18n.t("form.errors.minLength", {
+            field: i18n.t("form.password"),
+            length: 8,
+          }),
+        }),
+      confirmPassword: z
+        .string()
+        .min(1, {
+          message: i18n.t("form.errors.confirmPasswordRequired"),
+        })
+        .min(8, {
+          message: i18n.t("form.errors.minLength", {
+            field: i18n.t("form.confirmPassword"),
+            length: 8,
+          }),
+        }),
     })
     .superRefine(({ confirmPassword, password }, ctx) => {
       if (password !== confirmPassword) {
