b785603554f10840a11054c3c7b8789547c1e882
Germán Ramos <german.ramos@lightit.io>
2025-12-18 16:47:04 -0300

    refactor: using size class

--- src/routes/_private/providers/-components/-provider/provider-card-skeleton.tsx
+++ src/routes/_private/providers/-components/-provider/provider-card-skeleton.tsx
@@ -12,7 +12,7 @@ export const ProviderCardSkeleton = () => {
 
           <div className="flex flex-col gap-3">
             <div className="flex items-center gap-2">
-              <div className="h-6 w-6 animate-pulse rounded-full bg-background-surface" />
+              <div className="size-6 animate-pulse rounded-full bg-background-surface" />
               <div className="h-6 w-2/3 animate-pulse rounded-md bg-background-surface" />
             </div>
 
